bcd074fd968ded89f39d15828b46d0942a42b4c8
Michail Melonas <michail@rampgrowth.com>
2023-08-22 17:53:17 +0200

    Update learning rate and save model state after each epoch

--- fine_tune.py
+++ fine_tune.py
@@ -7,9 +7,9 @@ from utils import train, validate, calculate_wer
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-LR = 0.0005
+LR = 0.0000001
 BATCH_SIZE = 16
-EPOCHS = 5
+EPOCHS = 3
 
 train_dataset = PizzaSpeechDataset(train=True, device=DEVICE)
 val_dataset = PizzaSpeechDataset(train=False, device=DEVICE)
@@ -35,7 +35,7 @@ libri_speech_val_data_loader = torch.utils.data.DataLoader(
     collate_fn=collator,
 )
 
-model = whisper.load_model("base.en")
+model = whisper.load_model("medium.en")
 
 loss_fn = nn.CrossEntropyLoss(ignore_index=IGNORE_TOKEN)
 optimizer = torch.optim.AdamW(
@@ -52,3 +52,4 @@ for i in range(EPOCHS):
     val_wer = calculate_wer(model, val_data_loader)
     libri_speech_wer = calculate_wer(model, libri_speech_val_data_loader)
     print(f"Epoch {i + 1} WER: {val_wer}; Libri Speech WER: {libri_speech_wer}")
+    torch.save(model.state_dict(), f"models/fine-tuned-base-en-epoch-{i+1}.pth")
